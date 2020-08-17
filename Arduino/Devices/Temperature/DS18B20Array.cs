d62f88e720a764fc9883dfdbd6a9a4d1405af657
billchetch <45006134+billchetch@users.noreply.github.com>
2020-08-17 12:09:13 +0800

    ss

--- Arduino/Devices/Temperature/DS18B20Array.cs
+++ Arduino/Devices/Temperature/DS18B20Array.cs
@@ -21,8 +21,7 @@ namespace Chetch.Arduino.Devices.Temperature
             //Also note we are not using PULLUP mode as on testing this didn't work ... requirement is therefore
             //to use a 4.7K resistor bridging the data and power wires to the device.
             ConfigurePin(_oneWirePin, Solid.Arduino.Firmata.PinMode.DigitalInput);
-
-
+            
             TryAddCommand("read-temp", ArduinoCommand.CommandType.READ);
         }
 
@@ -35,25 +34,6 @@ namespace Chetch.Arduino.Devices.Temperature
 
             //this should be argument with index 3 (argument values are retrieved by numerical index on the arduino)
             message.AddArgument(_oneWirePin);
-        }
-
-        public override void HandleMessage(ADMMessage message)
-        {
-            base.HandleMessage(message);
-
-            //TODO: if data from read-temp then split into multiple messages
-        }
-
-        protected override void OnConnect(ADMMessage message)
-        {
-            base.OnConnect(message);
-
-            if (!message.HasValue("SensorCount"))
-            {
-                throw new Exception("DS18B20Array:OnConnect No SensorCount found");
-            }
-
-            // = message.GetInt("SIDX");
         }
     }
 }
