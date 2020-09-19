322955bfeb3b0b5f6254710cf97e4192b56055e4
billchetch <45006134+billchetch@users.noreply.github.com>
2020-09-19 15:37:44 +0800

    h

--- Arduino/Devices/Temperature/DS18B20Array.cs
+++ Arduino/Devices/Temperature/DS18B20Array.cs
@@ -8,6 +8,8 @@ namespace Chetch.Arduino.Devices.Temperature
 {
     public class DS18B20Array : TemperatureSensorBase
     {
+        public const String COMMAND_READ_TEMP = "read-temp";
+
         private int _oneWirePin;
         
         public DS18B20Array(int oneWirePin, String id) : base(id, "DS18B20")
@@ -17,17 +19,17 @@ namespace Chetch.Arduino.Devices.Temperature
             //at time of writing (16/08/2020) the firmata support for OneWire was unclear...
             //so the solution is to have OneWire + DallastTemperatures libs installed on the board
             //and send ADM messages of termperature that include the index of the sensor
-            //for handling by the computer (see HandleMessage below)...
+            //for handling by the computer...
             //Also note we are not using PULLUP mode as on testing this didn't work ... requirement is therefore
             //to use a 4.7K resistor bridging the data and power wires to the device.
             ConfigurePin(_oneWirePin, Solid.Arduino.Firmata.PinMode.DigitalInput);
 
-            TryAddCommand("read-temp", ArduinoCommand.CommandType.READ, true);
+            TryAddCommand(COMMAND_READ_TEMP, ArduinoCommand.CommandType.READ, true);
         }
 
         public DS18B20Array(int oneWirePin) : this(oneWirePin, "ds18" + oneWirePin){}
 
-
+        
         public override void AddConfig(ADMMessage message)
         {
             base.AddConfig(message);
