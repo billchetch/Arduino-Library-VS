6035ad77e81774c0915013687241de95258a9085
billchetch <45006134+billchetch@users.noreply.github.com>
2020-10-09 13:52:19 +0800

    s

--- Arduino/ArduinoDeviceGroup.cs
+++ Arduino/ArduinoDeviceGroup.cs
@@ -8,10 +8,34 @@ namespace Chetch.Arduino
 {
     public class ArduinoDeviceGroup
     {
-        public ArduinoDeviceManager ADM { get; set; };
+        public ArduinoDeviceManager ADM { get; set; }
         public String ID { get; internal set; }
         public String Name { get; internal set; }
 
         public List<ArduinoDevice> Devices { get; internal set; }
-    }
+
+        public ArduinoDeviceGroup(String id, String name)
+        {
+            ID = id;
+            Name = name;
+        }
+
+        public void AddDevice(ArduinoDevice dev)
+        {
+            if (!Devices.Contains(dev))
+            {
+                Devices.Add(dev);
+            }
+        }
+        
+        public ArduinoDevice GetDevice(String deviceID)
+        {
+            foreach(var dev in Devices)
+            {
+                if (dev.ID != null && dev.ID.Equals(deviceID)) return dev;
+            }
+            return null;
+        }
+
+    } //end class
 }
